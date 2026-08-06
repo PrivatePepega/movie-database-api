9e770e07b0649d215fbd8600a5b23d1aef3bef11
privatepepega <privatepepega@yahoo.com>
2026-08-06 11:42:34 -0400

    init prisma, init typescript

---
+++ src/config/db.js
@@ -0,0 +1,30 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ["error"], 
+});
+
+
+const connectDB = async () =>{
+    try {
+        await prisma.$connect();
+        console.log("Connected to database");
+    } catch (error) {
+        console.log("Error connecting to database", error);
+        process.exit(1);
+    }
+
+}
+
+const disconnectDB = async () =>{
+    try {
+        await prisma.$disconnect();
+        console.log("Disconnected from database");
+    } catch (error) {
+        console.log("Error disconnecting from database", error);
+        process.exit(1);
+    }
+
+}
+
+export { prisma, connectDB, disconnectDB };
