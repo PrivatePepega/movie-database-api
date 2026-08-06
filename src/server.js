c17e6be8e863650f8cc91a049e6bc0bc07226c61
privatepepega <privatepepega@yahoo.com>
2026-08-06 10:32:01 -0400

    first endpoint

--- src/server.js
+++ src/server.js
@@ -3,9 +3,12 @@ import express from 'express';
 
 
 const app = express();
-
-
 const PORT = process.env.PORT || 5001;
+
+
+app.get("/hello", (req, res) =>{
+    res.send("Hello World");
+})
 
 
 const server = app.listen(PORT, () =>{
