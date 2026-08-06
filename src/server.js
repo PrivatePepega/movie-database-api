e2bad824690fac09ed2698d5946a72a41452a757
privatepepega <privatepepega@yahoo.com>
2026-08-06 10:18:09 -0400

    server online

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+
+
+const app = express();
+
+
+const PORT = process.env.PORT || 5001;
+
+
+const server = app.listen(PORT, () =>{
+    console.log("Server online on port,", PORT);
+})
